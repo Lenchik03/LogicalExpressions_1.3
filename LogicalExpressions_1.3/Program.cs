d3f3b4a9a0bde9d9d92b0f277c068e2d2ef673a6
Polchik <pyrina200052004@mail.ru>
2023-10-30 19:59:22 +1000

    Добавьте файлы проекта.

---
+++ LogicalExpressions_1.3/Program.cs
@@ -0,0 +1,131 @@
+﻿
+//задание 3
+//int. TryParse(Console.ReadLine(), out int x);
+//int.TryParse(Console.ReadLine(), out int y);
+//int.TryParse(Console.ReadLine(), out int m);
+//int.TryParse(Console.ReadLine(), out int n);
+
+//if (m < x && x < n )
+//{
+//    Console.WriteLine("true");
+//}
+//else if(m>n)
+//    Console.WriteLine("ошибка");
+//else
+//    Console.WriteLine("false");
+
+//задание 4
+//int.TryParse(Console.ReadLine(), out int n);
+
+//double n2 = Math.Pow(n, 2);
+
+//int a = n / 100;
+//int b = n % 100 / 10;
+//int c = n % 10;
+//int s = a + b + c;
+//double k = Math.Pow(s, 3);
+
+//if (n2 == k)
+//    Console.WriteLine("true");
+//else
+//    Console.WriteLine("false");
+
+//задание 5
+//int.TryParse(Console.ReadLine(), out int n);
+
+//if (n >= 10 && n <= 99 && n % 2 == 0)
+//    Console.WriteLine("true");
+//else
+//    Console.WriteLine("false");
+
+//задание 6
+//int.TryParse(Console.ReadLine(), out int a);
+//int.TryParse(Console.ReadLine(), out int b);
+//int.TryParse(Console.ReadLine(), out int c);
+
+//if (a == b && b == c && c == a)
+//    Console.WriteLine("true");
+//else
+//    Console.WriteLine("false");
+
+//задание 7
+//int.TryParse(Console.ReadLine(), out int a);
+//int.TryParse(Console.ReadLine(), out int b);
+//int.TryParse(Console.ReadLine(), out int c);
+
+//if (a == b || a == c || b == c)
+//    Console.WriteLine("true");
+//else if (a == b && b == c && c == a)
+//    Console.WriteLine("он равносторонний");
+//else
+//    Console.WriteLine("false");
+
+//задание 8
+//int.TryParse(Console.ReadLine(), out int a);
+//int.TryParse(Console.ReadLine(), out int b);
+//int.TryParse(Console.ReadLine(), out int c);
+
+
+//if (a == -b || a == -c || b == -c)
+//    Console.WriteLine("true");
+//else 
+//    Console.WriteLine("false");
+
+//задание 9
+//int.TryParse(Console.ReadLine(), out int a);
+//int.TryParse(Console.ReadLine(), out int b);
+//int.TryParse(Console.ReadLine(), out int c);
+//int.TryParse(Console.ReadLine(), out int d);
+
+//if (a / c == b / d)
+//    Console.WriteLine("true");
+//else 
+//    Console.WriteLine("false");
+
+// задание 10
+//int.TryParse(Console.ReadLine(), out int a);
+//int.TryParse(Console.ReadLine(), out int b);
+//int.TryParse(Console.ReadLine(), out int c);
+
+//int.TryParse(Console.ReadLine(), out int a1);
+//int.TryParse(Console.ReadLine(), out int b1);
+//int.TryParse(Console.ReadLine(), out int c1);
+
+//int p1 = (a + b + c) / 2;
+//double S1 = Math.Sqrt(p1*(p1 - a)*(p1 - b)*(p1 - c));
+
+//int p2 = (a1 + b1 + c1) / 2;
+//double S2 = Math.Sqrt(p2*(p2 - a)*(p2 - b)*(p2 - c));
+
+//if (S1 == S2)
+//    Console.WriteLine("true");
+//else
+//    Console.WriteLine("false");
+
+//задание 11
+//int.TryParse(Console.ReadLine(), out int a);
+//int.TryParse(Console.ReadLine(), out int b);
+//int.TryParse(Console.ReadLine(), out int c);
+
+//if (Math.Pow(c, 2) == Math.Pow(a, 2) + Math.Pow(b, 2))
+//    Console.WriteLine("true");
+//else 
+//    Console.WriteLine("false")
+
+// задание 12
+//int.TryParse(Console.ReadLine(), out int n);
+
+//int a = n / 100;
+//int b = n / 100 % 10;
+//int c = n % 100 / 10;
+//int d = n % 10;
+
+//if(a!=b && a!=c && a!=d && b!=c && b!=d && c!=d)
+//    Console.WriteLine("true");
+//else 
+
+//    Console.WriteLine("false");
+
+
+//Залевская Полина и Никитина Алёна
+
